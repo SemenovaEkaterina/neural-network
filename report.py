7e10310dbfeb8d4d437826de38d6e3d683ac60f5
kate <kat-semyonova@mail.ru>
2017-11-21 22:58:03 +0300

    change report

--- report.py
+++ report.py
@@ -4,7 +4,11 @@ from dominate.tags import *
 doc = dominate.document(title='Report')
 
 
-a = 80
+stat = list()
+
+for i in range(10):
+    file = open('report/stat/{}'.format(i))
+    stat.append(file.read().split(';'))
 
 with doc.head:
     link(rel='stylesheet', href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')
@@ -16,17 +20,17 @@ with doc:
     with div(id='header'):
         with div(cls='row'):
             for i in range(10):
-                with div(cls='col-md-6'):
-                    with div(cls='panel panel-success'):
+                with div(cls='col-md-3'):
+                    with div(cls='panel panel-{}'.format('success' if (float(stat[i][0]) > 0.8) else 'danger')):
                         div('Header', cls='panel-heading')
                         with div(cls='panel-body'):
-                            with div(cls='col-md-5'):
-                                img('', src='imgs/%s.png' % i, width='200px', height='200px')
                             with div(cls='col-md-7'):
+                                img('', src='imgs/%s.png' % i, width='200px', height='170px')
+                            with div(cls='col-md-5'):
                                 with div(cls='panel panel-default'):
                                     with ul(cls='list-group'):
-                                        li('200', cls='list-group-item')
-                                        li('80%', cls='list-group-item')
+                                        li('{}%'.format(round(float(stat[i][0]),2) * 100), cls='list-group-item')
+                                        li('Total: {}'.format(stat[i][1]), cls='list-group-item')
 
 
 file = open('report/report.html', 'w')
